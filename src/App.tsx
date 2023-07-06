ce5b986f4a225e27bc0493e07ecc6ba7ba8651d3
HayatoKTYM <hayakap1211@gmail.com>
2023-07-06 09:32:12 +0900

    add new docs

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider, CSSReset, Box } from '@chakra-ui/react';
+import { RecoilRoot } from 'recoil';
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ChakraProvider>
+			<CSSReset />
+			<RecoilRoot>
+				<Box maxW={{ base: '100%', md: '80%', lg: '60%' }} m="auto" p={4}>
+					<TaskForm />
+					<TaskList />
+				</Box>
+			</RecoilRoot>
+		</ChakraProvider>
+	);
 }
 
 export default App;
